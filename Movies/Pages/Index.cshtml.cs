990a7b2bd841cf59ddf4eee3ec918ee14de74ac4
Robbieo <Robbieo@F1092-11-2099>
2019-11-20 14:05:20 -0600

    Caught up to the class.

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -9,19 +9,48 @@ namespace Movies.Pages
 {
     public class IndexModel : PageModel
     {
-        MovieDatabase movieDatabase = new MovieDatabase();
 
-        public List<Movie> Movies = new List<Movie>();
+        public MovieDatabase MovieDatabase = new MovieDatabase();
+
+        public List<Movie> Movies;
+
+        [BindProperty]
+        public string search { get; set; }
+
+        [BindProperty]
+        public List<string> mpaa { get; set; } = new List<string>();
+
+        [BindProperty]
+        public float? minIMDB { get; set; }
+
         public void OnGet()
         {
-            Movies = movieDatabase.All;
+            Movies = MovieDatabase.All;
         }
 
-        public void OnPost(string search)
+        public void OnPost()
         {
-            if (search != null)
+            if (search != null && mpaa.Count > 0)
             {
-                Movies = movieDatabase.Search(search);
+                Movies = MovieDatabase.Search(search);
+                Movies = MovieDatabase.FilterByMPAA(Movies, mpaa);
+            }
+            else if (search != null)
+            {
+                Movies = MovieDatabase.Search(search);
+            }
+            else if (mpaa.Count > 0)
+            {
+                Movies = MovieDatabase.FilterByMPAA(MovieDatabase.All, mpaa);
+            }
+            else
+            {
+                Movies = MovieDatabase.All;
+            }
+
+            if (minIMDB is float min)
+            {
+                Movies = MovieDatabase.FilterByMinIMDB(Movies, min);
             }
         }
     }
